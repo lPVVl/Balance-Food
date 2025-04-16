965539b0731728a383937fc3d716ba33cdcc040e
Vlad Petrichic <101114878+lPVVl@users.noreply.github.com>
2025-04-16 14:32:00 +0000

    добавление функицонала модального окна при скроле в конец страницы

--- js/script.js
+++ js/script.js
@@ -106,31 +106,45 @@ window.addEventListener('DOMContentLoaded', () => {
         modal = document.querySelector('.modal'),
         modalCloseBtn = document.querySelector('[data-close]');
 
+    function openModal() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow = 'hidden';
+        clearInterval(modalTimerId);
+    }
+
     modalTrigger.forEach(btn => {
-        btn.addEventListener('click', () => {
-            modal.classList.add('show');
-            modal.classList.remove('hide');
-            document.body.style.overflow = 'hidden'
-        });
+        btn.addEventListener('click', openModal);
     });
 
     function closeModal() {
         modal.classList.add('hide');
         modal.classList.remove('show');
-        document.body.style.overflow = ''
-    }
+        document.body.style.overflow = '';
+    };
 
-    modalCloseBtn.addEventListener('click', closeModal)
+    modalCloseBtn.addEventListener('click', closeModal);
 
     modal.addEventListener('click', (e) => {
         if (e.target === modal) {
-            closeModal()
-        }
+            closeModal();
+        };
     });
 
     document.addEventListener('keydown', (e) => {
         if (e.code === 'Escape' && modal.classList.contains('show')) {
-            closeModal()
+            closeModal();
         }
     });
+
+    const modalTimerId = setTimeout(openModal, 100000);
+
+    function showModalByScroll () {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+            openModal();
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
 });
