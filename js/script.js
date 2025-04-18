ccdc2e3ef147d9a45e530e37f2f5ff0648a482a9
Vlad Petrichic <101114878+lPVVl@users.noreply.github.com>
2025-04-18 14:04:24 +0000

    создание фунционала получения данных из формы

--- js/script.js
+++ js/script.js
@@ -216,5 +216,44 @@ window.addEventListener('DOMContentLoaded', () => {
         ".menu .container",
     ).render()
 
+    // forms
 
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        success: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    }
+
+    forms.forEach(item => {
+        postData(item);
+    })
+
+    function postData(form) {
+        form.addEventListener('submit', (e) => {
+            e.preventDafult();
+
+            const statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage)
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+            
+            const formData = new FormData(form);
+
+            request.send(formData);
+            
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response);
+                    statusMessage.textContent = message.success;
+                } else {
+                    statusMessage.textContent = message.failure;
+                }
+            });
+        });
+    }
 });
